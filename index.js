e1d7e6ee02a0ab3933cb8b7d9ec2915008500604
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-04 15:19:24 -0500

    Add index.js file

---
+++ index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+
+const rod = new MissionCommander('Rodrigo')
+console.log(rod.name)
+
+const fer = new MissionCommander('Fernanda')
+console.log(fer.name)
