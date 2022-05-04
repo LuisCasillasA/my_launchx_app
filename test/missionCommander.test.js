a00bbd58e7921a9d2c4e5aa6e1fc1f51deda8af2
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-04 15:30:19 -0500

    Repair mission commander test

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,7 +1,7 @@
 describe("This is a test suite", () => {
     test('Test case 1', () => {
       const result = 1 + 2 
-      expect(result).toBe(10);
+      expect(result).toBe(3);
     });
   })
   
